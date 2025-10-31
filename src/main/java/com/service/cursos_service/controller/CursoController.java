642e2c11117a29139f65d22e357c508b51c03a3b
adria <yagorubio2@gmail.com>
2025-10-31 16:57:21 -0500

    modificaciones

--- src/main/java/com/service/cursos_service/controller/CursoController.java
+++ src/main/java/com/service/cursos_service/controller/CursoController.java
@@ -1,4 +1,31 @@
 package com.service.cursos_service.controller;
 
+
+import com.service.cursos_service.model.Curso;
+import com.service.cursos_service.service.CursoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
 public class CursoController {
+    private final CursoService cursoService;
+
+    @GetMapping("/cursos/listar")
+    public List<Curso> ListarCurso(){
+        try{
+            return cursoService.obtenerCursos();
+        }
+        catch(Exception ex)
+        {
+            return  List.of();
+        }
+    }
+
+
 }
